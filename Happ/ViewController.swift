1e223f6f7e2de59584b2a4967ed9a275ad61ca15
Sattar Stamkulov <sattar.coder@gmail.com>
2016-09-05 11:54:45 +0600

    Initial Commit

---
+++ Happ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happ
+//
+//  Created by MacBook Pro on 9/5/16.
+//  Copyright © 2016 Sattar Stamkulov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
