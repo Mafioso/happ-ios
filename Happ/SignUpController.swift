fd0d9eb862a4d6340ef8d3253f0d0d2fb7ae162e
katayevaa <kataeva.aigerim@gmail.com>
2016-09-07 14:17:39 +0600

    signUpController added

--- Happ/SignUpController.swift
+++ Happ/SignUpController.swift
@@ -1,41 +1,43 @@
 //
-//  ViewController.swift
+//  SignUpController.swift
 //  Happ
 //
-//  Created by MacBook Pro on 9/5/16.
+//  Created by Aigerim'sMac on 07.09.16.
 //  Copyright © 2016 Sattar Stamkulov. All rights reserved.
 //
 
 import UIKit
 
+class SignUpController: UIViewController {
 
-class LoginController: UIViewController {
-
-    // outlets
     @IBOutlet weak var constraintSignInFormBottom: NSLayoutConstraint!
     @IBOutlet weak var constraintSignInToBottomContainer: NSLayoutConstraint!
-
-    @IBOutlet weak var viewBottomContainer: UIView!
-    @IBOutlet weak var appLogoImageView: UIImageView!
     
-    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var bottomContainerView: UIView!
     @IBOutlet weak var enterWithFbButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
     
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    
+    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+
+        // Do any additional setup after loading the view.
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg-image")!)
-        signInButton.layer.cornerRadius = 5
-        signInButton.layer.masksToBounds = true
         
         
-        // textfield icon position 
+        signUpButton.layer.cornerRadius = 5
+        signUpButton.layer.masksToBounds = true
+        
+        
+        // textfield icon position
         let leftImageView1 = UIImageView()
         leftImageView1.image = UIImage(named: "username-icon")
         let leftView1 = UIView()
@@ -54,11 +56,18 @@ class LoginController: UIViewController {
         passwordTextField.leftView = leftView2
         passwordTextField.leftViewMode = UITextFieldViewMode.Always
         
+        let leftImageView3 = UIImageView()
+        leftImageView3.image = UIImage(named: "password-icon")
+        let leftView3 = UIView()
+        leftView3.addSubview(leftImageView3)
+        leftView3.frame = CGRectMake(0, 0, 20, 20)
+        leftImageView3.frame = CGRectMake(3, 0, 15, 15)
+        repeatPasswordTextField.leftView = leftView3
+        repeatPasswordTextField.leftViewMode = UITextFieldViewMode.Always
         
         
         
         
-
         // init observers
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(LoginController.keyboardWillShow(_:)), name:UIKeyboardWillShowNotification, object: nil);
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(LoginController.keyboardWillHide(_:)), name:UIKeyboardWillHideNotification, object: nil);
@@ -66,16 +75,18 @@ class LoginController: UIViewController {
         // init
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(LoginController.dismissKeyboard))
         view.addGestureRecognizer(tap)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     override func viewDidDisappear(animated: Bool) {
         super.viewDidDisappear(animated)
-
+        
         // remove observer
         NSNotificationCenter.defaultCenter().removeObserver(self)
     }
@@ -88,29 +99,41 @@ class LoginController: UIViewController {
         maskLayer.path = path.CGPath
         enterWithFbButton.layer.mask = maskLayer
     }
-
-
+    
+    
     func keyboardWillShow(notification: NSNotification) {
         let info = notification.userInfo!
         let keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
-
-        self.viewBottomContainer.hidden = true
+        
+        self.bottomContainerView.hidden = true
         self.constraintSignInToBottomContainer.active = false
-
+        
         self.constraintSignInFormBottom.constant  = keyboardFrame.size.height + 20
         self.constraintSignInFormBottom.active = true
     }
-
+    
     func keyboardWillHide(notification: NSNotification) {
-
-        self.viewBottomContainer.hidden = false
+        
+        self.bottomContainerView.hidden = false
         self.constraintSignInToBottomContainer.active = true
-
+        
         self.constraintSignInFormBottom.active = false
     }
-
-
+    
+    
     func dismissKeyboard() {
         view.endEditing(true)
     }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
---
+++ Happ/UILabel+SubstituteFont.swift
@@ -0,0 +1,20 @@
+//
+//  UILabel+SubstituteFont.swift
+//  Happ
+//
+//  Created by Aigerim'sMac on 07.09.16.
+//  Copyright © 2016 Sattar Stamkulov. All rights reserved.
+//
+
+import UIKit
+
+//extension to substitute fonts in entire project
+
+extension UILabel {
+    
+    var substituteFontName : String {
+        get { return self.font.fontName }
+        set { self.font = UIFont(name: newValue, size: self.font.pointSize) }
+    }
+    
+}
