0881ae2085140a9a3c37193f9a586b846549a6f1
katayevaa <kataeva.aigerim@gmail.com>
2016-09-26 17:54:09 +0600

    side bar menu added

--- Happ/UserService.swift
+++ Happ/UserService.swift
@@ -46,7 +46,7 @@ class UserService {
     // check for valid credential, fetch updated if was expired
     class func isCredentialAvailable() -> Promise<Bool> {
 
-        UserService.deleteCredential()
+        //UserService.deleteCredential()
 
         return Promise { fulfill, reject in
             if let credential = UserService.getCredential() {
