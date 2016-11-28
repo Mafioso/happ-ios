b4b6a54ec7336783f90f75cf3d6def5499621eda
Sattar Stamkulov <sattar.coder@gmail.com>
2016-11-29 02:01:59 +0600

    added Empty List Controller

--- Happ/EventsListEmptyViewController.swift
+++ Happ/EventsListEmptyViewController.swift
@@ -10,13 +10,20 @@
 import UIKit
 
 
-
 class EventsListEmptyViewController: UIViewController {
 
     var viewModel: EventsListViewModel! {
         didSet {
             self.bindToViewModel()
         }
+    }
+
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var labelDescription: UILabel!
+    @IBOutlet weak var buttonAction: UIButton!
+    
+    @IBAction func clickedActionButton(sender: UIButton) {
     }
 
 
@@ -26,6 +33,11 @@ class EventsListEmptyViewController: UIViewController {
         self.extMakeNavBarWhite()
         self.initNavBarItems()
     }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        buttonAction.extMakeCircle()
+    }
 
 
     func viewModelDidUpdate() {
@@ -34,8 +46,56 @@ class EventsListEmptyViewController: UIViewController {
             !state.events.isEmpty {
             self.navigationController?.popViewControllerAnimated(true)
         }
+        
+        buttonAction.titleLabel?.text = self.getActionTitle()
+        buttonAction.imageView?.image = self.getActionIcon()
+        labelDescription.text = self.getDescription()
     }
 
+    
+    private func getDescription() -> String {
+        switch self.viewModel.state.scope {
+        case .Favourite:
+            return "You don’t have any favourited event"
+        case .Feed:
+            return "There are no events for selected interests"
+        default:
+            fatalError()
+        }
+    }
+    private func getActionTitle() -> String {
+        switch self.viewModel.state.scope {
+        case .Favourite:
+            return "Find Awesome Events"
+        case .Feed:
+            return "Add More Interests"
+        default:
+            fatalError()
+        }
+    }
+    private func getActionIcon() -> UIImage {
+        switch self.viewModel.state.scope {
+        case .Favourite:
+            return UIImage(named: "icon-star")!
+        case .Feed:
+            return UIImage(named: "icon-search")!
+        default:
+            fatalError()
+        }
+    }
+    private func handleClickAction() {
+        switch self.viewModel.state.scope {
+        case .Favourite:
+            // self.viewModel.navigateFeed?()
+            break
+        case .Feed:
+            // self.viewModel.navigateSelectInterests?()
+            break
+        default:
+            fatalError()
+        }
+    }
+    
 
     private func bindToViewModel() {
         let superDidUpdate = self.viewModel.didUpdate
